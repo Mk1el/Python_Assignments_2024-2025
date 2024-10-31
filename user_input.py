5d36c210fb54f2ee5d0b7aa5be835d222cd40a0b
Mk1el <moseskilonzo2001@gmail.com>
2024-10-31 15:53:53 +0300

    Week 1 python assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name")
+age = input("Enter your age")
+location = input("Enter your location")
+
+print(f"Hello, my name is {name}, I am {age} years old and I stay in {location}")
